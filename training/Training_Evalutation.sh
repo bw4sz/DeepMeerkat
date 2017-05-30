6f983995d6363108f967733a12aa1488c6186d40
bw4sz <benweinstein2010@gmail.com>
2017-05-30 14:31:01 -0700

    no print statements for annotations

--- training/Training_Evalutation.sh
+++ training/Training_Evalutation.sh
@@ -16,7 +16,10 @@ declare -r MODEL_NAME="DeepMeerkat"
 declare -r JOB_ID="${MODEL_NAME}_$(date +%Y%m%d_%H%M%S)"
 declare -r GCS_PATH="${BUCKET}/${MODEL_NAME}/${JOB_ID}"
 
-#from scratch
+############
+#Train Model
+############
+
 python pipeline.py \
     --project ${PROJECT} \
     --cloud \
@@ -27,9 +30,10 @@ python pipeline.py \
     --gcs_bucket ${BUCKET} \
     --output_dir "${GCS_PATH}/" \
     --sample_image_uri  gs://api-project-773889352370-ml/Hummingbirds/Positives/10000.jpg  
-    
-#Run evaluation predictions 
+
+################    
 #Mount directory
+################
 
 #make empty directory for mount
 mkdir /mnt/gcs-bucket
@@ -37,7 +41,7 @@ mkdir /mnt/gcs-bucket
 #give it permissions
 chmod a+w /mnt/gcs-bucket
 
-#MOUNT 
+#GCSFUSE
 gcsfuse --implicit-dirs api-project-773889352370-ml ~/mnt/gcs-bucket
 
 ##########################
@@ -54,18 +58,19 @@ cat /mnt/gcs-bucket/Hummingbirds/testingdata.csv  | cut -f 1 -d "," | head -n 20
 sed "s|gs://api-project-773889352370-ml/|/mnt/gcs-bucket/|g" eval_files.txt  > jpgs.txt
 
 #Batch prediction
+JOB_NAME="${MODEL_NAME}_$(date +%Y%m%d_%H%M%S)"
 JSON_INSTANCES=Instances_$(date +%Y%m%d_%H%M%S).json
-python images_to_json.py -o $JSON_INSTANCES $(cat jpgs.txt)
-gsutil cp $JSON_INSTANCES gs://api-project-773889352370-ml/Hummingbirds/Prediction/
 
-JOB_NAME=predict_Meerkat_$(date +%Y%m%d_%H%M%S)
+#Embed images as .json
+python images_to_json.py -o $JSON_INSTANCES $(cat jpgs.txt)
+gsutil cp $JSON_INSTANCES gs://api-project-773889352370-ml/Hummingbirds/Prediction/$JOB_NAME/
+
 gcloud ml-engine jobs submit prediction $JOB_NAME \
     --model=$MODEL_NAME \
     --data-format=TEXT \
-    --input-paths=gs://api-project-773889352370-ml/Hummingbirds/Prediction/$JSON_INSTANCES \
-    --output-path=gs://api-project-773889352370-ml/Hummingbirds/Prediction/ \
+    --input-paths=gs://api-project-773889352370-ml/Hummingbirds/Prediction/$JOB_NAME/$JSON_INSTANCES \
+    --output-path=gs://api-project-773889352370-ml/Hummingbirds/Prediction/$JOB_NAME/ \
     --region=us-central1
-
 
 ##########################
 #Out of sample predictions
@@ -76,19 +81,22 @@ head trainingdata.csv | cut -d ',' -f1 > eval.csv
 
 #extract eval frames to predict
 cat /mnt/gcs-bucket/Hummingbirds/holdoutdata.csv  | cut -f 1 -d "," | head -n 20 > eval_files.txt
+
 #fix local mount path
 sed "s|gs://api-project-773889352370-ml/|/mnt/gcs-bucket/|g" eval_files.txt  > jpgs.txt
 
 #Batch prediction
+JOB_NAME_holdout="${MODEL_NAME}_$(date +%Y%m%d_%H%M%S)"
 JSON_INSTANCES=Instances_$(date +%Y%m%d_%H%M%S).json
-python images_to_json.py -o $JSON_INSTANCES $(cat jpgs.txt)
-gsutil cp $JSON_INSTANCES gs://api-project-773889352370-ml/Hummingbirds/Prediction/
 
-JOB_NAME_holdout=predict_Meerkat_$(date +%Y%m%d_%H%M%S)
+#Embed images as .json
+python images_to_json.py -o $JSON_INSTANCES $(cat jpgs.txt)
+gsutil cp $JSON_INSTANCES gs://api-project-773889352370-ml/Hummingbirds/Prediction/$JOB_NAME_holdout/
+
 gcloud ml-engine jobs submit prediction $JOB_NAME_holdout \
     --model=$MODEL_NAME \
     --data-format=TEXT \
-    --input-paths=gs://api-project-773889352370-ml/Hummingbirds/Prediction/$JOB_NAME_holdout \
+    --input-paths=gs://api-project-773889352370-ml/Hummingbirds/Prediction/$JOB_NAME_holdout/$JSON_INSTANCES \
     --output-path=gs://api-project-773889352370-ml/Hummingbirds/Prediction/$JOB_NAME_holdout \
     --region=us-central1
 
@@ -97,14 +105,14 @@ gcloud ml-engine jobs stream-logs $JOB_NAME_holdout
 
 #holdout
 #Parse predictions and enter information into database, key, prediction, run, date
-python ParsePredictions.py -input /mnt/gcs-bucket/Hummingbirds/Prediction/$JOB_NAME -ouput $JOB_NAME
+python ParsePredictions.py -input /mnt/gcs-bucket/Hummingbirds/Prediction/$JOB_NAME/ -ouput $JOB_NAME
 
-gsutil cp $JOB_NAME.csv gs://api-project-773889352370-ml/Hummingbirds/Prediction
+gsutil cp $JOB_NAME.csv gs://api-project-773889352370-ml/Hummingbirds/Prediction/Summary/
 
 #eva;
-python ParsePredictions.py -input /mnt/gcs-bucket/Hummingbirds/Prediction/$JOB_NAME_holdout -ouput $JOB_NAME_holdout
+python ParsePredictions.py -input /mnt/gcs-bucket/Hummingbirds/Prediction/$JOB_NAME_holdout/ -ouput $JOB_NAME_holdout
 
-gsutil cp $JOB_NAME_holdout.csv gs://api-project-773889352370-ml/Hummingbirds/Prediction
+gsutil cp $JOB_NAME_holdout.csv gs://api-project-773889352370-ml/Hummingbirds/Prediction/Summary
 
 
 exit
