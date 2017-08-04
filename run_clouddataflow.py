4c89649626726a146aacf7c1f442a66cc35cc24a
bw4sz <benweinstein2010@gmail.com>
2017-08-04 11:13:54 -0700

    last path

--- run_clouddataflow.py
+++ run_clouddataflow.py
@@ -27,8 +27,7 @@ class PredictDoFn(beam.DoFn):
     bucket = storage_client.get_bucket(parsed.hostname)
     blob=storage.Blob(parsed.path[1:],bucket)
     print(blob.exists())
-    local_path="/tmp/" + parsed.path[1:]
-    
+    local_path="/tmp/" + "video.avi"
     with open(local_path, 'wb') as file_obj:
       blob.download_to_file(file_obj)
     
