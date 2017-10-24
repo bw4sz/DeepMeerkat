f8999fc3bd7ec93675543cbc86764bfebb0a8331
bw4sz <benweinstein2010@gmail.com>
2017-10-24 13:45:24 -0700

    version 0.0.2

--- training/Classification/GenerateAnnotate_new.py
+++ training/Classification/GenerateAnnotate_new.py
@@ -4,7 +4,7 @@ import random
 import os
 2
 cv2.namedWindow("image")
-images=glob.glob("/Users/ben/Dropbox/GoogleCloud/TestCrops/Positives/*.jpg")
+images=glob.glob("/Users/ben/Dropbox/GoogleCloud/TestCrops/Negatives/*.jpg")
 position=0
 
 for path in images:
