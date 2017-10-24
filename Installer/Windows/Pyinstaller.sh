1259733af978184593de54930cb560f5b4c8909d
bw4sz <benweinstein2010@gmail.com>
2017-10-24 11:15:12 -0700

    local windows change

--- Installer/Windows/Pyinstaller.sh
+++ Installer/Windows/Pyinstaller.sh
@@ -3,7 +3,7 @@ rm -rf C:/Users/Ben/Documents/DeepMeerkat/Installer/Windows/dis
 rm -rf C:/Users/Ben/Documents/DeepMeerkat/Installer/Windows/build 
 rm -rf C:/Users/Ben/Documents/DeepMeerkat/Installer/Windows/Output 
 
-/c/Python35/Scripts/pyinstaller -c DeepMeerkat.spec
+/c/Python35/Scripts/pyinstaller -c --windowed -y DeepMeerkat.spec
 
 #copy model
 cp -r C:/Users/ben/Dropbox/GoogleCloud/DeepMeerkat_20170924_105144/model dist/Lib/
