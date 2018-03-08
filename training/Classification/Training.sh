f8e6d3d60e26400f26f74264d82d6894c9f8cfda
bw4sz <benweinstein2010@gmail.com>
2018-03-08 13:26:36 -0800

    Revert "mistake"
    This reverts commit e741f8f41c3133a8dd1162979b25dde82dcb3208.

--- training/Classification/Training.sh
+++ training/Classification/Training.sh
@@ -4,13 +4,17 @@
 source env/bin/activate
 
 #make sure all requirements are upgraded
-pip install -r requirements.txt
+#pip install -r requirements.txt
 
 declare  PROJECT=$(gcloud config list project --format "value(core.project)")
 declare  BUCKET="gs://${PROJECT}-ml"
 declare  MODEL_NAME="DeepMeerkat"
 declare  JOB_ID="${MODEL_NAME}_$(date +%Y%m%d_%H%M%S)"
 declare  GCS_PATH="${BUCKET}/${MODEL_NAME}/${JOB_ID}"
+
+#make sure paths are updated
+gsutil rsync -d /Users/Ben/Dropbox/GoogleCloud/Training/Positives/ gs://api-project-773889352370-ml/Hummingbirds/Training/Positives
+gsutil rsync -d /Users/Ben/Dropbox/GoogleCloud/Training/Negatives/ gs://api-project-773889352370-ml/Hummingbirds/Training/Negatives
 
 #get eval set size
 eval=$(gsutil cat gs://api-project-773889352370-ml/Hummingbirds/testingdata.csv | wc -l)
@@ -24,11 +28,28 @@ python CreateDocs.py
 
 python pipeline.py \
     --project ${PROJECT} \
+    --cloud \
     --train_input_path gs://api-project-773889352370-ml/Hummingbirds/trainingdata.csv \
     --eval_input_path gs://api-project-773889352370-ml/Hummingbirds/testingdata.csv \
     --input_dict gs://api-project-773889352370-ml/Hummingbirds/dict.txt \
+    --deploy_model_name "DeepMeerkat" \
     --gcs_bucket ${BUCKET} \
-    --output_dir 
+    --output_dir "${GCS_PATH}/"  \
+    --eval_set_size  ${eval} 
+
+
+##already preprocessed
+#python pipeline.py \
+    #--project ${PROJECT} \
+    #--cloud \
+    #--preprocessed_train_set gs://api-project-773889352370-ml/DeepMeerkat/DeepMeerkat_20171011_134826/preprocessed/train* \
+    #--preprocessed_eval_set gs://api-project-773889352370-ml/DeepMeerkat/DeepMeerkat_20171011_134826/preprocessed/eval* \
+    #--input_dict gs://api-project-773889352370-ml/Hummingbirds/dict.txt \
+    #--deploy_model_name "DeepMeerkat" \
+    #--gcs_bucket ${BUCKET} \
+    #--output_dir "${GCS_PATH}/" \
+    #--eval_set_size  ${eval} 
+    
     
 #Monitor
 tensorboard --logdir ${GCS_PATH} 
